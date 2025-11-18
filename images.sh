6a8b10f72019923af02c451152b6223b19d79ad3
解(xie)先生🌽 <chenglongxie@foxmail.com>
2025-11-18 13:33:29 +0800

    增加镜像构建脚本

---
+++ images.sh
@@ -0,0 +1,3 @@
+docker build -f console\frontend\Dockerfile -t harbor.bigdata.com/agentic/agentic-platform-agent/console-frontend:latest .
+
+docker build -f console\backend\hub\Dockerfile -t harbor.bigdata.com/agentic/agentic-platform-agent/console-hub:latest .
