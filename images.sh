8b94f2d7db185ba6f009218894ec781189503f3e
解(xie)先生🌽 <chenglongxie@foxmail.com>
2025-11-24 08:02:04 +0800

    变更镜像名

--- images.sh
+++ images.sh
@@ -5,13 +5,35 @@ docker build -f console\backend\hub\Dockerfile -t harbor.bigdata.com/agentic/age
 ## 拉取镜像
 docker pull ghcr.io/9999-12-31/agentic-platform-agent/console-frontend:latest
 docker pull ghcr.io/9999-12-31/agentic-platform-agent/console-hub:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-rpa:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-agent:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-database:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-knowledge:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-workflow:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-link:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-tenant:latest
+docker pull ghcr.io/9999-12-31/agentic-platform-agent/core-aitools:latest
 
 ## 重命名镜像为内网镜像
 docker tag ghcr.io/9999-12-31/agentic-platform-agent/console-frontend:latest harbor.bigdata.com/agentic/agentic-platform-agent/console-frontend:latest
 docker tag ghcr.io/9999-12-31/agentic-platform-agent/console-hub:latest harbor.bigdata.com/agentic/agentic-platform-agent/console-hub:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-rpa:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-rpa:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-agent:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-agent:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-database:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-database:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-knowledge:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-knowledge:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-workflow:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-workflow:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-link:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-link:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-tenant:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-tenant:latest
+docker tag ghcr.io/9999-12-31/agentic-platform-agent/core-aitools:latest harbor.bigdata.com/agentic/agentic-platform-agent/core-aitools:latest
 
 ## 推送镜像
 docker push harbor.bigdata.com/agentic/agentic-platform-agent/console-frontend:latest
 docker push harbor.bigdata.com/agentic/agentic-platform-agent/console-hub:latest
-
-
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-rpa:latest
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-agent:latest
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-database:latest
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-knowledge:latest
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-workflow:latest
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-link:latest
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-tenant:latest
+docker push harbor.bigdata.com/agentic/agentic-platform-agent/core-aitools:latest
